6158fa963ae10d4aba44bb1cf5a453560b166e74
Mauro Alves <maurojr_@outlook.com>
2021-07-04 18:47:06 -0300

    Adicionando função iniciarRelogio

--- js/main.js
+++ js/main.js
@@ -6,17 +6,33 @@ const botaoParar = document.querySelector("#botao-parar");
 let duracaoSessao = 1500;
 let tempoRestanteNaSessao = 3600;
 let duracaoPausaSessao = 300;
-
+            
 let relogioRodando = false;
 
 botaoIniciar.addEventListener("click", () => {
-
+    iniciarRelogio();
 });
 
 botaoPausar.addEventListener("click", () => {
-
+    iniciarRelogio();
 });
 
 botaoParar.addEventListener("click", () => {
-    
+    iniciarRelogio(true);
 });
+
+const iniciarRelogio = (reset) => {
+    if (reset) {
+    } else {
+      if (relogioRodando === true) {
+        clearInterval(relogioTimer);
+        relogioRodando = false;
+      } else {
+        relogioRodando = true;
+        relogioTimer = setInterval(() => {
+          tempoRestanteNaSessao--;
+          exibirTempoRestante();
+        }, 1000);
+      }
+    }
+};
