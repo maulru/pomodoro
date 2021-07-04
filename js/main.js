ff8e148e8828f7b494ba14ea915f7a42b705a545
Mauro Alves <maurojr_@outlook.com>
2021-07-04 20:54:56 -0300

    Adicionado contador e som

--- js/main.js
+++ js/main.js
@@ -9,71 +9,83 @@ let tempoRestanteNaSessao = 1500;
 let duracaoPausaSessao = 300;
 
 let relogioRodando = false;
-
 let execucoes = 0;
+let modo = 0;
 
 botaoIniciar.addEventListener("click", () => {
-  iniciarRelogio();
+  verificaRelogio();
+  modo = 1;
 });
 
 botaoPausar.addEventListener("click", () => {
-  iniciarRelogio();
+  verificaRelogio();
 });
 
 botaoParar.addEventListener("click", () => {
-  iniciarRelogio(true);
+  verificaRelogio(true);
 });
 
 botaoIntervalo.addEventListener("click", () => {
   tempoRestanteNaSessao = duracaoPausaSessao;
-  iniciarRelogio();
+  verificaRelogio();
 });
 
-const iniciarRelogio = (reset) => {
+const verificaRelogio = (reset) => {
   if (reset) {
     pararRelogio();
   } else {
     if (relogioRodando === true) {
-      clearInterval(relogioTimer);
-      relogioRodando = false;
+      pausarRelogio();
     } else {
-      relogioRodando = true;
-      relogioTimer = setInterval(() => {
-        tempoRestanteNaSessao--;
-        exibirTempoRestante();
-      }, 1000);
+      iniciarRelogio();
     }
   }
 };
 
 const exibirTempoRestante = () => {
- if (tempoRestanteNaSessao >= 0){
-  const segundosRestantes = tempoRestanteNaSessao;
-  let resultado = "";
-  const segundos = segundosRestantes % 60;
-  const minutos = parseInt(segundosRestantes / 60) % 60;
-  let horas = parseInt(segundosRestantes / 3600);
+  if (tempoRestanteNaSessao >= 0) {
+    const segundosRestantes = tempoRestanteNaSessao;
+    let resultado = "";
+    const segundos = segundosRestantes % 60;
+    const minutos = parseInt(segundosRestantes / 60) % 60;
+    let horas = parseInt(segundosRestantes / 3600);
 
-  function adicionandoZeros(tempo) {
-    return tempo < 10 ? `0${tempo}` : tempo;
+    function adicionandoZeros(tempo) {
+      return tempo < 10 ? `0${tempo}` : tempo;
+    }
+
+    if (horas > 0) resultado += `${horas}`;
+    resultado += `${adicionandoZeros(minutos)}:${adicionandoZeros(segundos)}`;
+    contador.innerText = resultado.toString();
+  } else {
+    if (modo === 1) {
+      execucoes++;
+      modo = 0;
+    }
+    clearInterval(relogioTimer);
+    pomodoros.innerText = `Pomodoros realizados:${execucoes}`;
+    contador.innerText = "25:00";
+    var audio = new Audio("song/pare.mp3");
+    audio.play();
   }
-
-  if (horas > 0) resultado += `${horas}`;
-  resultado += `${adicionandoZeros(minutos)}:${adicionandoZeros(segundos)}`;
-  contador.innerText = resultado.toString();
- }else{
-  execucoes++;
-  clearInterval(relogioTimer);
-  pomodoros.innerText = `Pomodoros realizados:${execucoes}`
- }
-
- console.log(tempoRestanteNaSessao);
-
 };
 
-function pararRelogio(){
+function pararRelogio() {
   relogioRodando = false;
   clearInterval(relogioTimer);
   tempoRestanteNaSessao = 1500;
   contador.innerText = "25:00";
+}
+
+function pausarRelogio() {
+  clearInterval(relogioTimer);
+  relogioRodando = false;
+}
+
+function iniciarRelogio() {
+  relogioRodando = true;
+  relogioTimer = setInterval(() => {
+    tempoRestanteNaSessao--;
+    exibirTempoRestante();
+  }, 1000);
 }
